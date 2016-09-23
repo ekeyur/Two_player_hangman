0eff6cb78b0fee8d383c283b8f64a7d5f72c8b87
Keyur Patel <ekeyur@gmail.com>
2016-09-23 10:28:44 -0400

    Fixed the hangman drawing code again

--- hang_man.py
+++ hang_man.py
@@ -1,7 +1,5 @@
 import getpass
-
 # string_ha =""
-
 guess = ''
 
 count = 6
@@ -10,9 +8,9 @@ str_print = ""
 
 def hanged(ct):
     if ct == 6:
-        frame [0][2] = "|"
+        frame [0][3] = "|"
     elif ct == 5:
-        frame[1][2] = " O"
+        frame[1][2] = "O"
     elif ct == 4:
         frame[2][2] = "|"
     elif ct == 3:
@@ -30,15 +28,14 @@ def hanged(ct):
 
 while (play_again == "Y"):
     count = 6
-    frame = [[" --","--","--","--","|"," "],
-            ["  "," "," ","  ","  |"," "],
+    frame = [["---","-|","---","-","|"," "],
+            ["  ","  ","  ","  ","  |"," "],
             ["   "," "," ","   "," |"," "],
             ["   "," "," "," ","   |"," "],
             ["   "," "," ","  ","  |"," "],
             ["  ","  ","  ","---"," ","---"]]
 
-    string_ha = getpass.getpass(prompt='Player 1: Enter a word for hangman game: ')
-    hang_man_str = str(string_ha)
+    hang_man_str = getpass.getpass(prompt='Player 1: Enter a word for hangman game: ')
     guessed = ['_']*len(hang_man_str)
     while (count > 0) and (hang_man_str != ''.join(guessed)):
         guess = raw_input("Player 2: Guess a letter in the word: ")
@@ -57,6 +54,6 @@ while (play_again == "Y"):
     if((''.join(guessed)) == hang_man_str):
         print "Player 2: Good Job. You guessed the string correctly."
     else:
-        print "Player 2: You suck at guessing!, go read a dictionary. Player 1 entered %s as the secret hangman word. " % string_ha
+        print "Player 2: You suck at guessing!, go read a dictionary. Player 1 entered %s as the secret hangman word. " % hang_man_str
 # Play again
     play_again = raw_input("Do you want to try your luck again. (Y or N)")
